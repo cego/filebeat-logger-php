5b7cab5b806de50be7dd311f36bc0f71adf0d532
Mads Jon Nielsen <madsjon@gmail.com>
2022-06-17 07:54:52 +0200

    IP variable is always set, and use client.addresss ECS field as well

--- src/FilebeatContextProcessor.php
+++ src/FilebeatContextProcessor.php
@@ -134,7 +134,8 @@ class FilebeatContextProcessor
             $record['context']['client'] = [];
         }
 
-        $record['context']['client']['ip'] = isset($ip) ? explode(',', $ip)[0] : null;
+        $record['context']['client']['ip'] = explode(',', $ip);
+        $record['context']['client']['address'] = $ip;
 
         return $record;
     }
